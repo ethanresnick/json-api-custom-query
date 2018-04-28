e1bb92d085bc5d5b593915e327b02720e33378ac
Ethan Resnick <ethan.resnick@gmail.com>
2018-04-27 22:32:17 -0400

    fix(types): fix parser types
    These had gotten a bit out of sync

--- src/parser.d.ts
+++ src/parser.d.ts
@@ -1,24 +1,29 @@
 declare namespace Parser {
   export type Identifier = { type: "identifier", value: string };
   export type Atom = number | string | boolean | null | Identifier;
-  export type Value = Atom | List;
+  export type Value = Atom | List | RawFieldExpression;
 
   // List is an interface as a hack for it being recursive.
   // See https://github.com/Microsoft/TypeScript/issues/3496
   interface List extends Array<Value> {}
 
+  interface RawFieldExpression {
+    type: "RawFieldExpression",
+    items: Value[]
+  }
+
   // The parse for sort fields has a raw List in the expression field,
   // as we have no contextual info to convert it to anything else.
-  export type SortField = ({
+  export type RawSortField = ({
     field: string
   } | {
-    expression: List
+    expression: RawFieldExpression
   }) & {
     direction: "ASC" | "DESC"
   };
 
-  export function parse(input: string, opts: { startRule: "Sort" }): SortField[]
-  export function parse(input: string, opts: { startRule: "Filter" }): List[]
+  export function parse(input: string, opts: { startRule: "Sort" }): RawSortField[]
+  export function parse(input: string, opts: { startRule: "Filter" }): RawFieldExpression[]
 }
 
 export = Parser;
